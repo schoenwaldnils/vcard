f33dea807b823cbd285a64e98621f72a1705cad5
Nils Schönwald <nils@schoen.world>
2019-11-02 19:38:51 +0100

    remove js files from head

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,49 @@
+/* eslint-disable max-classes-per-file */
 import React from 'react'
 import Document, { Head, Main } from 'next/document'
+
+class CustomHead extends Head {
+  render() {
+    const res = super.render()
+
+    function transform(node) {
+      // remove all link preloads
+      if (
+        node &&
+        node.type === 'link' &&
+        node.props &&
+        node.props.rel === 'preload'
+      ) {
+        return null
+      }
+      if (node && node.props && node.props.children) {
+        console.log(node.props.children)
+        return {
+          ...node,
+          props: {
+            ...node.props,
+            children: Array.isArray(node.props.children)
+              ? node.props.children.map(transform)
+              : transform(node.props.children),
+          },
+        }
+      }
+      if (Array.isArray(node)) {
+        return node.map(transform)
+      }
+
+      return node
+    }
+
+    return transform(res)
+  }
+}
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html lang="de">
-        <Head />
+        <CustomHead />
         <body>
           <Main />
         </body>

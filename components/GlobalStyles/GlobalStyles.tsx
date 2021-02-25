e3a8ae61944a99fdd9ab310d72cdfaa7be72cdc9
Nils Schönwald <nils@schoenwald.media>
2021-02-25 10:03:48 +0100

    remove open sans

--- components/GlobalStyles/GlobalStyles.tsx
+++ components/GlobalStyles/GlobalStyles.tsx
@@ -2,13 +2,11 @@ import { Global } from '@emotion/react'
 import { FC } from 'react'
 
 import base from './base'
-import fonts from './fonts'
 import normalize from './normalize'
 
 export const GlobalStyles: FC = () => (
   <>
     <Global styles={normalize} />
-    <Global styles={fonts} />
     <Global styles={base} />
   </>
 )
